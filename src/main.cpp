f71366c124e8263eb5de625ee6b6c72f3489120f
Jaku6M <j.mazurkiewicz13@wp.pl>
2025-01-28 19:11:50 +0100

    Initial commit with working blink program

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <Arduino.h>
+
+const int ledPin = 2;
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(5000);
+  digitalWrite(ledPin, LOW);  
+  delay(5000);
+}
