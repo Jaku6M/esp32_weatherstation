30b360255913bf9df96a1080163f271f8fd8ce56
Jaku6M <j.mazurkiewicz13@wp.pl>
2025-01-29 22:47:10 +0100

    Added DHT sensor functionality (not working rn)

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,31 @@
 #include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+DHT dht(26, DHT21);
 
 const int ledPin = 2;
 void setup() {
   pinMode(ledPin, OUTPUT);
+
+  dht.begin();
+  delay(2000);
+
+  Serial1.begin(9600);
 }
 
 void loop() {
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
   digitalWrite(ledPin, HIGH);
-  delay(5000);
+  Serial1.print("Temperature: ");
+  Serial1.print(temperature);
+  Serial1.print("°C");
+  Serial1.print(" Humidity: ");
+  Serial1.print(humidity);
+  Serial1.println("%");
+  delay(1000);
   digitalWrite(ledPin, LOW);  
-  delay(5000);
+  delay(1000);
 }
